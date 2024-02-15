61714fb321ab0ae0a08e7d4dc4279d2e38de61a6
Tcampbell93 <tcampbell_93@hotmail.com>
2024-02-15 10:00:30 -0800

    added questions

--- index.js
+++ index.js
@@ -4,7 +4,48 @@ const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'projectName',
+        message: 'What is the title of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Give a description of your project.'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What are the installation instructions for your project?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use your project?'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'List any collaborators or contributions you had for this project.'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Write any test instructions you have for your project.'
+    },
+    {
+        type: 'input',
+        name: 'username',
+        message: 'What is your Github username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
